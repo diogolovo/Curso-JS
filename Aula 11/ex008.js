ba00755aadf8cd0cb42df09f182ee72493d50cca
Diogo Lovo <67477103+diogolovo@users.noreply.github.com>
2020-06-28 22:25:33 -0300

    Começando a brincar com js

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do automovel e ${vel} km/h`)
+if (vel > 60) { //Condição simples
+    console.log(`Sua velocidade foi maior que 60km, MULTADO!`)
+} 
+console.log(`Dirija sempre usando o cinto de seguramça!`)
---
+++ Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Você morrando no ${país}`)
+if (país == 'Brasil') {
+    console.log('Você e Brasileiro')
+} else {
+    console.log('Você e estrageiro')
+}
