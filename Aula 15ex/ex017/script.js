7ff3b349056ff0e725d831d0e556bab9a571fd38
Diogo Lovo <67477103+diogolovo@users.noreply.github.com>
2020-07-02 17:23:34 -0300

    Termino do Módulo E

---
+++ Aula 15ex/ex017/script.js
@@ -0,0 +1,42 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    //Caixa de texto
+    let tab = document.getElementById('seltab')
+    //Select
+    
+    if (num.value.length == 0) {
+        alert('Coloca o número animal!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+
+/*function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Animal, coloca o número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+}*/
+
